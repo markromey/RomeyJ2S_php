50c943021978ce9d141755e251b1078ac6886617
markromey <123348942+markromey@users.noreply.github.com>
2023-02-06 18:34:57 +0800

    created database
    i created database

---
+++ databaseconnect.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "Tundra";
+$password = "akositundra123";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->close();
+?>
